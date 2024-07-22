088638369b12c899bde21261bf6ba06da0bfce40
george <georgezambonin@gmail.com>
2024-07-22 11:13:38 -0400

    load_run added

--- app.py
+++ app.py
@@ -13,9 +13,25 @@ import gymnasium
 def get_device():
     return "cuda" if th.cuda.is_available() else "cpu"
 
+def load_run():
+
+    model = RecurrentPPO.load('model.zip')
+    env = gymnasium.make("ALE/Enduro-v5", obs_type="grayscale", render_mode="human", full_action_space=True)
+    env = EnvWrapper(env=env, reward_predictor=None, seq_len=30)
+    obs, _ = env.reset()
+    # cell and hidden state of the LSTM
+    lstm_states = None
+    num_envs = 1
+    # Episode start signals are used to reset the lstm states
+    episode_starts = np.ones((num_envs,), dtype=bool)
+    while True:
+        action, lstm_states = model.predict(obs, state=lstm_states, episode_start=episode_starts)#, deterministic=True)
+        obs, rewards, dones, truncated, info = env.step(action)
+        episode_starts = dones
+        env.render()
 def main():
     
-    env = gymnasium.make("ALE/Enduro-v5", obs_type="grayscale", full_action_space=True)
+    env = gymnasium.make("ALE/Enduro-v5", obs_type="grayscale", render_mode="human", full_action_space=True)
     device = get_device()
     reward_model = RewardPredictor((1,80,80), 5, n_action=env.action_space.n, device=device)
     
@@ -27,31 +43,16 @@ def main():
     )
 
     model = RecurrentPPO(CnnLstmPolicy, env, policy_kwargs=policy_kwargs,
-                         n_steps=1024,
+                         n_steps=512,
                          batch_size=64,
                          verbose=2,
                          learning_rate=1e-4)
     #print(model.policy)
 
     callback = TrainRewardPredictorCallback(reward_model)
-    model.learn(100_000, callback=callback)
+    model.learn(10_000, callback=callback)
     model.save('model')
-    
-    env = gymnasium.make("ALE/Enduro-v5", obs_type="grayscale", render_mode="human", full_action_space=True)
-    env = EnvWrapper(env=env, reward_predictor=reward_model, seq_len=30)
-
-    obs, _ = env.reset()
-    # cell and hidden state of the LSTM
-    lstm_states = None
-    num_envs = 1
-    # Episode start signals are used to reset the lstm states
-    episode_starts = np.ones((num_envs,), dtype=bool)
-    while True:
-        action, lstm_states = model.predict(obs, state=lstm_states, episode_start=episode_starts, deterministic=True)
-        obs, rewards, dones, truncated, info = env.step(action)
-        episode_starts = dones
-        env.render()
-
 
 if __name__ == "__main__":
-    main()
+    # main()
+    load_run()
